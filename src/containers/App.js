185bfca899a4e57a106c1fc9839c8cca9c0f52db
Harry Bedford <hbedford@scottlogic.co.uk>
2019-08-06 17:04:19 +0100

    Introduce redirect

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import StartPage from './StartPage';
 import RunningPage from './RunningPage';
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 const App = () => {
   return (
     <Switch>
       <Route exact path="/" component={StartPage} />
       <Route path="/running" component={RunningPage} />
+      <Redirect to="/" />
     </Switch>
   );
 }
