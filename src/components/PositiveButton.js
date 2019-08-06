f958ee3132bf6e2422e169dcb36fd35ea3e53523
Harry Bedford <hbedford@scottlogic.co.uk>
2019-08-06 17:04:05 +0100

    Update button's styling

--- src/components/PositiveButton.js
+++ src/components/PositiveButton.js
@@ -4,7 +4,7 @@ import styles from './PositiveButton.module.css';
 export default class PositiveButton extends Component {    
     render() {
         return (
-            <button className={styles.posBtn}>
+            <button disabled={this.props.disabled} className={styles.posBtn}>
                 {this.props.text}
             </button>
         )
