c4a789e4ea74442e127e36f56d39ced5676eff6d
Harry Bedford <hbedford@scottlogic.co.uk>
2019-08-12 09:31:14 +0100

    work in progress to validate settings

--- src/containers/ConfigPage.js
+++ src/containers/ConfigPage.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 import PositiveButton from '../components/PositiveButton';
 import { Link } from 'react-router-dom';
-import styles from './ConfigPage.module.css'
 import TimeSettings from './TimeSettings';
 import Exercises from './Exercises';
 import { bindActionCreators } from 'redux';
-import * as actions from '../actions/actions';
 import { connect } from 'react-redux';
+import styles from './ConfigPage.module.css';
+
+import * as actions from '../actions/actions';
 
 class ConfigPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            goLink: styles.invalidLink
+        }
+        this.isValid = this.isValid.bind(this);
+    }
+
+    // Why is this method not being called?
+    isValid() {
+        const { exercises, workTime, restTime } = this.props
+        if (exercises.length > 0) {
+            console.log("validLink")   
+            return true
+        } else {
+            console.log("invalid link")
+            return false
+        }
+    }
+
+    // componentDidUpdate() {
+    //     console.log(this.state.goLink)
+    //     // const test = this.isValid()
+    //     //     ? styles.validLink
+    //     //     : styles.invalidLink
+    //     this.setState({goLink: styles.validLink})
+    // }
+
     render() {
         return (
             <div className={styles.page}>
@@ -25,8 +54,8 @@ class ConfigPage extends Component {
                         restTime={this.props.restTime}
                         updateTime={this.props.updateTime}
                     />
-                    <Link to="/running" >
-                        <PositiveButton icon={'angle right'} disabled={false} />
+                    <Link to="/running" className={this.isValid ? styles.validLink : styles.invalidLink} >
+                        <PositiveButton icon={'angle right'} />
                     </Link>
                 </div>
             </div>
