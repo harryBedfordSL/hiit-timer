c943515eff1946fcf790033506904f21c39312ee
Harry Bedford <hbedford@scottlogic.co.uk>
2019-08-12 16:35:26 +0100

    Adjust time settings to account for new buttons

--- src/containers/TimeSettings.js
+++ src/containers/TimeSettings.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import styles from './TimeSettings.module.css';
 import actionType from '../actions/types';
+import NumberInput from '../components/NumberInput';
 
 export default class TimeSettings extends Component {
-
     constructor(props) {
         super(props);
-
         this.handleChange = this.handleChange.bind(this);
+        this.increment = this.increment.bind(this);
     }
 
     handleChange(event) {
@@ -16,29 +15,34 @@ export default class TimeSettings extends Component {
         this.props.updateTime(key, value);
     }
 
+    increment(direction, target, prevValue) {
+        if (direction === 'minus') {
+            this.props.updateTime(target, prevValue-1)
+        } else if (direction == 'plus') {
+            this.props.updateTime(target, prevValue+1)
+        }
+    }
+
     render() {
         return (
             <div>
-                Work time: <br/>
-                <input 
+                <NumberInput
+                    inputTitle='Work time:' 
                     id={actionType.UPDATE_WORK_TIME}
-                    className={styles.input}
-                    type="number"
                     min="0"
                     max="60"
                     value={this.props.workTime}
                     onChange={this.handleChange}
+                    increment={this.increment}
                 />
-                <br/><br/>
-                Rest time: <br/>
-                <input
+                <NumberInput
+                    inputTitle='Rest time:' 
                     id={actionType.UPDATE_REST_TIME}
-                    className={styles.input}
-                    type="number"
                     min="0"
                     max="60"
                     value={this.props.restTime}
-                    onChange={this.handleChange}    
+                    onChange={this.handleChange}
+                    increment={this.increment} 
                 />
             </div>
         )
