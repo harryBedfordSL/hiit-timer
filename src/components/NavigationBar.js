1b346d1f35ffe270d3b1546a1458f2d7591cf864
Harry Bedford <hbedford@scottlogic.co.uk>
2019-08-27 14:13:22 +0100

    Introduce nac bar component

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import NeutralButton from './buttons/NeutralButton';
+import PositiveButton from './buttons/PositiveButton';
+
+import styles from './NavigationBar.module.css';
+
+export default class NavigationBar extends Component {
+    render() {
+        const { isSettingValid, back, next } = this.props;
+        return (
+            <div className={styles.navigation}>
+                <Link to={back}>
+                    <NeutralButton
+                        icon={'angle left'}
+                        size='big'
+                    />
+                </Link>
+                <Link to={next} className={isSettingValid ? styles.validLink : styles.invalidLink} >
+                    <PositiveButton
+                        icon={'angle right'}
+                        size='big'
+                    />
+                </Link>
+            </div>
+        )
+    }
+}
