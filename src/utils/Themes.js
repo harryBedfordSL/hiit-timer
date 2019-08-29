e3c276a722d4edcaed169c278adde695e8fcbc7d
Harry Bedford <hbedford@scottlogic.co.uk>
2019-08-29 15:46:21 +0100

    Make reducer for updating theme and separate out initial states

---
+++ src/utils/Themes.js
@@ -0,0 +1,45 @@
+export const LIGHT_THEME = {
+    mode: 'light',
+    page: {
+        backgroundColor: 'white',
+        color: 'black'
+    },
+    modal: {
+        backgroundColor: '#c4c4c4',
+    },
+    input: {
+        backgroundColor: '#d0d0d0'
+    },
+    neutralButton: {
+        backgroundColor: '#a3a3a3'
+    },
+    positiveButton: {
+        backgroundColor: '#598a5f'
+    },
+    negativeButton: {
+        backgroundColor: '#893939de'
+    }
+}
+
+export const DARK_THEME = {
+    mode: 'dark',
+    page: {
+        backgroundColor: '#282c34',
+        color: 'white'
+    },
+    modal: {
+        backgroundColor: '#464646',
+    },
+    input: {
+        backgroundColor: 'white'
+    },
+    neutralButton: {
+        backgroundColor: '#53565d'
+    },
+    positiveButton: {
+        backgroundColor: '#598a5fb7'
+    },
+    negativeButton: {
+        backgroundColor: '#893939b7'
+    }
+}
